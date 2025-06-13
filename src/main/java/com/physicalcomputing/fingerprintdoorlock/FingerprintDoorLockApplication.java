0070b6b6adf526b74e424bab2a3983e574fdaeec
LEEBYEONGHWA <qudghk4084@naver.com>
2025-06-13 13:40:20 +0900

    chore: initialize spring

---
+++ src/main/java/com/physicalcomputing/fingerprintdoorlock/FingerprintDoorLockApplication.java
@@ -0,0 +1,13 @@
+package com.physicalcomputing.fingerprintdoorlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerprintDoorLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingerprintDoorLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/physicalcomputing/fingerprintdoorlock/FingerprintDoorLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.physicalcomputing.fingerprintdoorlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingerprintDoorLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
