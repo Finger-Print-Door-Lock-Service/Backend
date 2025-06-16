de8c6d6f38d6dcff07d0f34c7f695d308b3d4e69
LEEBYEONGHWA <qudghk4084@naver.com>
2025-06-16 19:36:52 +0900

    implementation for register device for mqtt and db

--- src/main/java/com/physicalcomputing/fingerprintdoorlock/controller/DeviceRegisterController.java
+++ src/main/java/com/physicalcomputing/fingerprintdoorlock/controller/DeviceRegisterController.java
@@ -1,30 +1,40 @@
 package com.physicalcomputing.fingerprintdoorlock.controller;
 
 import com.physicalcomputing.fingerprintdoorlock.domain.dto.ArduinoDTO;
+import com.physicalcomputing.fingerprintdoorlock.domain.dto.ArduinoRequestDTO;
+import com.physicalcomputing.fingerprintdoorlock.mqtt.MqttService;
 import com.physicalcomputing.fingerprintdoorlock.service.deviceService.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/devices")
 public class DeviceRegisterController {
 
     private final DeviceService deviceService;
+    private final MqttService mqttService;
 
     // 로그인 page show
     @GetMapping
-    public String showLoginPage() {
+    public String showLoginPage(Model model) {
+        model.addAttribute("deviceId", new ArduinoRequestDTO.DeviceRegisterDTO());
         return "devices/login";
     }
 
-    // 디바이스 등록 버튼을 누르면 실행
-    @PostMapping("/device")
-    public String register(@RequestBody ArduinoDTO.DeviceRegisterDTO deviceRegisterDTO){
-        deviceService.registerDevice(deviceRegisterDTO);
-        return null;
+    // 이 버튼은 db 저장용이 아니라 mqtt 연결용입니다.
+    @PostMapping("/mqtt")
+    public void registerDevicesForMqtt(@ModelAttribute ArduinoRequestDTO.DeviceRegisterDTO deviceRegisterDTO) {
+        int deviceNumForMqtt = deviceRegisterDTO.getDeviceNumForMqtt();
+        String topic = "fingerprint/" + deviceNumForMqtt;
+        mqttService.publish(topic, "connected with Server");
     }
 
+    // esp 32에서 등록 완료 후 -> spring (db 등록을 위해서)
+    @PostMapping("/device")
+    public void registerDevice(@RequestBody ArduinoDTO.DeviceRegisterDTO deviceRegisterDTO) {
+        deviceService.registerDevice(deviceRegisterDTO);
+    }
 }
